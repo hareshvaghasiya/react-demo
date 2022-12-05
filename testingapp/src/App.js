0e16aee15acf34a527891391a6153cea9fb6b81b
hareshv-e2m <hareshv@e2msol.com>
2022-12-05 17:29:08 +0530

    Add files

---
+++ testingapp/src/App.js
@@ -0,0 +1,19 @@
+// import logo from './logo.svg';
+
+import './App.css';
+import Header from './Component/Header';
+import Footer from './Component/Footer';
+
+import './Component/Style.css';
+
+function App() {
+  return (
+  <>
+    <Header></Header>
+    
+    <Footer></Footer>
+  </>
+  )
+}
+
+export default App;
